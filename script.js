c757e6b7d603de1234c389e49d869d1cc7ccdc79
sergiozeppo <sp1tfire_90@mail.ru>
2024-01-07 18:45:35 +0600

    feat: implement game

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
+const body = document.querySelector("body");
 const virtualKeyboard = document.querySelector(".virtual-keyboard");
 const guess = document.querySelector(".guess-part b");
 const answerHidden = document.querySelector(".answer-hide");
 const alienHangman = document.querySelector(".gallows-part img");
+const modal = document.querySelector(".modal");
+const modalImg = modal.querySelector("img");
+const modalGreet = modal.querySelector("h3");
+const modalText = modal.querySelector("p");
+const modalAnswer = modalText.querySelector("b");
 
-let currentQuestion;
+let currentAnswer;
 let wrongTry = 0;
 let maxTry = 6;
+const lettersCorrect = [];
 
 function randomiser() {
   const { answer, hint } = questions[~~(Math.random() * questions.length)];
   console.log(answer);
-  currentQuestion = answer;
-  console.log(currentQuestion);
+  currentAnswer = answer;
+  console.log(currentAnswer);
   document.querySelector(".hint-part b").innerText = hint;
   answerHidden.innerHTML = answer
     .split("")
     .map(() => `<li class="letter"></li>`)
     .join("");
 }
+function gameOver(isWin) {
+  setTimeout(()=> {
+    modal.classList.add("visible");
+    modalImg.src = `./images/${isWin? "victory" : "lost"}.png`;
+    modalGreet.innerText = `${isWin? "You WIN!" : "You lost!"}`;
+    modalText.innerHTML = `${isWin? "You guessed the word correctly: " : "The correct answer was: "} <b>${currentAnswer}</b>`;}, 250)
+}
 function buttonCheck(button, letterClicked) {
-  if (currentQuestion.includes(letterClicked)) {
+  if (currentAnswer.includes(letterClicked)) {
     let current = answerHidden.querySelectorAll("li");
-    [...currentQuestion].forEach((letter, index) => {
+    [...currentAnswer].forEach((letter, index) => {
       if(letter === letterClicked) {
           current[index].innerText = letter;
           current[index].classList.add("correct");
+          lettersCorrect.push(letter);
       }
   });
   } else {
@@ -33,6 +48,8 @@ function buttonCheck(button, letterClicked) {
   }
   guess.innerText = `${wrongTry} / ${maxTry}`;
   button.disabled = true;
+  if (wrongTry===maxTry) return gameOver(false);
+  if (lettersCorrect.length===currentAnswer.length) return gameOver(true);
 }
 for (i = 97; i <= 122; i++) {
   const button = document.createElement("button");
