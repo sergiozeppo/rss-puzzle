ddbdf4038c2df47a305461c30c2c20e27dba69bd
sergiozeppo <sp1tfire_90@mail.ru>
2024-01-07 14:32:22 +0600

    feat: add questions, implement quiz mechanics

---
+++ script.js
@@ -0,0 +1,36 @@
+const virtualKeyboard = document.querySelector(".virtual-keyboard");
+const answerHidden = document.querySelector(".answer-hide");
+let currentQuestion;
+
+function randomiser() {
+  const { answer, hint } = questions[~~(Math.random() * questions.length)];
+  console.log(answer);
+  currentQuestion = answer;
+  console.log(currentQuestion);
+  document.querySelector(".hint-part b").innerText = hint;
+  answerHidden.innerHTML = answer
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+}
+function buttonCheck(button, letterClicked) {
+  if (currentQuestion.includes(letterClicked)) {
+    let current = answerHidden.querySelectorAll("li");
+    [...currentQuestion].forEach((letter, index) => {
+      if(letter === letterClicked) {
+          current[index].innerText = letter;
+          current[index].classList.add("correct");
+      }
+  });
+  } else {
+      console.log(letterClicked, "Letter is NOT in the word");
+  }
+  button.disabled = true;
+}
+for (i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  virtualKeyboard.appendChild(button);
+  button.addEventListener("click", (e) => buttonCheck(e.target, button.innerText.toLowerCase()));
+}
+randomiser();
