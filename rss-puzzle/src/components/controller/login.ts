c347667cd776a0100170d4bc5534a7c871aef9fd
Sergey Tsepodoy <149455058+sergiozeppo@users.noreply.github.com>
2024-03-09 18:05:43 +0500

    refactor: RSS-PZ-000 Refactor Code For DRY (#50)

--- rss-puzzle/src/components/controller/login.ts
+++ rss-puzzle/src/components/controller/login.ts
@@ -45,85 +45,38 @@ function checkDisableButton(): void {
   } else submitButton.disabled = false;
 }
 
+function validateError(dest: HTMLInputElement, input: string): void {
+  const nameError = new ValidationError(input);
+  dest.insertAdjacentHTML('afterend', `<p class="error-message">${nameError.message}</p>`);
+  setTimeout(() => {
+    const deleteError = document.querySelector('.error-message');
+    form?.removeChild(deleteError as Node);
+  }, 3000);
+  submitButton.disabled = true;
+}
+
 function checkName(): void {
-  if (!nameInput.value) {
-    const nameError = new ValidationError(`Please, enter ${nameInput.placeholder}`);
-    nameInput.insertAdjacentHTML('afterend', `<p class="error-message">${nameError.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
+  if (!nameInput.value) validateError(nameInput, `Please, enter ${nameInput.placeholder}`);
   if (nameInput.validity.tooShort) {
     const min = nameInput.getAttribute('minLength');
-    const nameError = new ValidationError(`The minimum length should be ${min}`);
-    nameInput.insertAdjacentHTML('afterend', `<p class="error-message">${nameError.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
+    validateError(nameInput, `The minimum length should be ${min}`);
   }
-  if (!nameInput.value.match(/^[A-Z]/g)) {
-    const nameError = new ValidationError(`First letter should be capital.`);
-    nameInput.insertAdjacentHTML('afterend', `<p class="error-message">${nameError.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
-  if (!nameInput.value.match(/[a-zA-z]/g)) {
-    const nameError = new ValidationError(`Only latin letters and hyphen allowed.`);
-    nameInput.insertAdjacentHTML('afterend', `<p class="error-message">${nameError.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
+  if (!nameInput.value.match(/^[A-Z]/g)) validateError(nameInput, `First letter should be capital`);
+  if (!nameInput.value.match(/[a-zA-z]/g))
+    validateError(nameInput, `Only latin letters and hyphen allowed`);
   checkDisableButton();
 }
 
 function checkSurname(): void {
-  if (!surnameInput.value) {
-    const error = new ValidationError(`Please, enter ${nameInput.placeholder}`);
-    surnameInput.insertAdjacentHTML('afterend', `<p class="error-message">${error.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
+  if (!surnameInput.value) validateError(surnameInput, `Please, enter ${nameInput.placeholder}`);
   if (surnameInput.validity.tooShort) {
     const min = surnameInput.getAttribute('minLength');
-    const error = new ValidationError(`The minimum length should be ${min}`);
-    surnameInput.insertAdjacentHTML('afterend', `<p class="error-message">${error.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
+    validateError(surnameInput, `The minimum length should be ${min}`);
   }
-  if (!surnameInput.value.match(/^[A-Z]/g)) {
-    const error = new ValidationError(`First letter should be capital.`);
-    surnameInput.insertAdjacentHTML('afterend', `<p class="error-message">${error.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
-  if (!surnameInput.value.match(/[a-zA-z]/g)) {
-    const error = new ValidationError(`Only latin letters and hyphen allowed.`);
-    surnameInput.insertAdjacentHTML('afterend', `<p class="error-message">${error.message}</p>`);
-    setTimeout(() => {
-      const deleteError = document.querySelector('.error-message');
-      form?.removeChild(deleteError as Node);
-    }, 3000);
-    submitButton.disabled = true;
-  }
+  if (!surnameInput.value.match(/^[A-Z]/g))
+    validateError(surnameInput, `First letter should be capital`);
+  if (!surnameInput.value.match(/[a-zA-z]/g))
+    validateError(surnameInput, `Only latin letters and hyphen allowed`);
   checkDisableButton();
 }
 
